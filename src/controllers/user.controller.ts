88b184b434b74e7d440d9b893e7116a1a6f8bf0a
hosainahmeed <skyfal430@gmail.com>
2025-11-04 17:33:24 +0600

    create uitle for code genarate

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,19 @@
 import User from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { codeGenerator } from "../utils/codeGenerator.js";
+import { validateFields } from "../utils/validateFields.js";
 
 const createUser = asyncHandler(async (req, res) => {
     try {
+        const payload = req.body
+        validateFields(payload, ['name', 'email', 'password'])
         const { name, email, password } = req.body;
-        if (!(name || email || password)) {
-            throw new ApiError(400, 'please provide all required fields')
-        }
-
         const existingUser = await User.findOne({ email })
         if (existingUser) {
             throw new ApiError(400, "Email address you are trying to use is already taken by another account")
         }
+        const { code, expiredAt } = codeGenerator(3)
         await User.create()
     } catch (error) {
 
---
+++ src/utils/codeGenerator.ts
@@ -0,0 +1,6 @@
+export const codeGenerator = (time: number) => {
+  const code = Math.floor(100000 + Math.random() * 900000).toString();
+  const expiredAt = Date.now() + time * 60 * 1000;
+  return { code, expiredAt };
+};
+
